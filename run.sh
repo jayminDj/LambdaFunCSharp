160817d6c5498c156d63d8cf9f6895a63b1e8847
jaymin <jaymin@promactinfo.com>
2017-10-16 12:43:17 +0530

    test

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+dotnet restore;
 if [ $CIRCLE_BRANCH = "master" -o $CIRCLE_BRANCH = "dev" -o $CIRCLE_BRANCH = "staging" ];
 then
   apt-get update && apt-get install python-pip zip unzip -y;
@@ -8,9 +8,7 @@ then
   aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID ;
   aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY ;
   aws configure set region us-east-1;
-  dotnet restore;
   dotnet lambda deploy-function LambdaFunCSharp --function-role lambda_basic_execution;
 else
-  dotnet restore;
   dotnet build;
 fi
