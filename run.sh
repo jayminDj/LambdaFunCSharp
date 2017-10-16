7c14486f8ad707eed5fb887130cf3564d3c9c053
jaymin <jaymin@promactinfo.com>
2017-10-16 12:16:29 +0530

    test

--- run.sh
+++ run.sh
@@ -2,17 +2,15 @@
 
 if [ $CIRCLE_BRANCH = "master" -o $CIRCLE_BRANCH = "dev" -o $CIRCLE_BRANCH = "staging" ];
 then
-  echo 'in dev';
-  #apt-get update && apt-get install python-pip zip unzip -y;
-  #pip install awscli;
-  #export PATH=~/.local/bin:$PATH;
-  #aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID ;
-  #aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY ;
-  #aws configure set region us-east-1;
-  #dotnet restore;
-  #dotnet lambda deploy-function LambdaFunCSharp --function-role lambda_basic_execution;
+  apt-get update && apt-get install python-pip zip unzip -y;
+  pip install awscli;
+  export PATH=~/.local/bin:$PATH;
+  aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID ;
+  aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY ;
+  aws configure set region us-east-1;
+  dotnet restore;
+  dotnet lambda deploy-function LambdaFunCSharp --function-role lambda_basic_execution;
 else
-  echo 'not in dev';
-  #dotnet restore;
-  #dotnet build;
+  dotnet restore;
+  dotnet build;
 fi
