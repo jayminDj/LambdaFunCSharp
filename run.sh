13e665fa575f95c3d02c1b70c24c08ccb1be9314
jaymin <jaymin@promactinfo.com>
2017-10-16 11:41:03 +0530

    test

--- run.sh
+++ run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 printenv;
-if [ $CIRCLE_BRANCH = "change" ]
+if [ $CIRCLE_BRANCH = "master" ] || [$CIRCLE_BRANCH = "dev"] || [$CIRCLE_BRANCH = "staging"]
 then
-    echo "change branch";
+  apt-get update && apt-get install python-pip zip unzip -y;
+  pip install awscli;
+  export PATH=~/.local/bin:$PATH;
+  aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+  aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+  aws configure set region us-east-1;
+  dotnet restore;
+  dotnet lambda deploy-function LambdaFunCSharp --function-role lambda_basic_execution;
 else
     echo "other branch";
 fi
-#apt-get update && apt-get install python-pip zip unzip -y;
-#pip install awscli;
-#export PATH=~/.local/bin:$PATH;
-#aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-#aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-#aws configure set region us-east-1;
-#dotnet restore;
-#dotnet lambda deploy-function LambdaFunCSharp --function-role lambda_basic_execution;
