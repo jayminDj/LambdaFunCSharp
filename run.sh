6f71da08a9ed93f65391bff66177d2b9bfb04439
jaymin <jaymin@promactinfo.com>
2017-10-16 10:51:35 +0530

    test

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-apt-get update && apt-get install python-pip zip unzip -y;
-pip install awscli;
-export PATH=~/.local/bin:$PATH;
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set region us-east-1;
-dotnet restore;
-dotnet lambda deploy-function LambdaFunCSharp --function-role lambda_basic_execution;
+printenv;
+#apt-get update && apt-get install python-pip zip unzip -y;
+#pip install awscli;
+#export PATH=~/.local/bin:$PATH;
+#aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+#aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+#aws configure set region us-east-1;
+#dotnet restore;
+#dotnet lambda deploy-function LambdaFunCSharp --function-role lambda_basic_execution;
