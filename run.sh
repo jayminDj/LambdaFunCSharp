ca1fcfe8951deea055aabed61e2943f8fcd4c3bb
jaymin <jaymin@promactinfo.com>
2017-10-18 11:10:00 +0530

    test

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+deploy_produnction(){
+  dotnet lambda deploy-function LambdaFunCSharpProd --region us-east-1 $*;
+}
+
 dotnet restore;
 if [ $CIRCLE_BRANCH = "master" -o $CIRCLE_BRANCH = "dev" -o $CIRCLE_BRANCH = "staging" ];
 then
@@ -10,14 +15,12 @@ then
   aws configure set region us-east-1;
   aws configure set output json
   Lambda_func_config='-frole arn:aws:iam::275485857435:role/user-removal-service-lambda -fsub subnet-a88614e0,subnet-55db070f --function-security-groups sg-b9b543c8';
-  deploy_produnction
+  deploy_produnction $Lambda_func_config
 else
   dotnet build;
 fi
 
-deploy_produnction(){
-  dotnet lambda deploy-function LambdaFunCSharpProd --region us-east-1 $*;
-}
+
 
 deploy_development(){
   dotnet lambda deploy-function LambdaFunCSharpDev --region us-east-1 -frole arn:aws:iam::275485857435:role/user-removal-service-lambda \
