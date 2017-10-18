ef0eb18b1fb08053d0918474867dd582fee08b34
jaymin <jaymin@promactinfo.com>
2017-10-18 11:15:56 +0530

    test

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ then
   aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY ;
   aws configure set region us-east-1;
   aws configure set output json
-  Lambda_func_config='-frole arn:aws:iam::275485857435:role/user-removal-service-lambda -fsub subnet-a88614e0,subnet-55db070f --function-security-groups sg-b9b543c8';
+  Lambda_func_config='-frun dotnetcore1.0 -frole arn:aws:iam::275485857435:role/user-removal-service-lambda -fsub subnet-a88614e0,subnet-55db070f --function-security-groups sg-b9b543c8';
   deploy_produnction $Lambda_func_config
 else
   dotnet build;
