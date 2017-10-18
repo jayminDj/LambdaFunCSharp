b8b0393285e9b2c68465b0bba2bb9845ab719ba4
jaymin <jaymin@promactinfo.com>
2017-10-18 11:22:31 +0530

    test

--- run.sh
+++ run.sh
@@ -4,6 +4,14 @@ deploy_produnction(){
   dotnet lambda deploy-function LambdaFunCSharpProd --region us-east-1 $*;
 }
 
+deploy_development(){
+  dotnet lambda deploy-function LambdaFunCSharpDev --region us-east-1 $*;
+}
+
+deploy_stagging(){
+  dotnet lambda deploy-function LambdaFunCSharpStag --region us-east-1 $*;
+}
+
 dotnet restore;
 if [ $CIRCLE_BRANCH = "master" -o $CIRCLE_BRANCH = "dev" -o $CIRCLE_BRANCH = "staging" ];
 then
@@ -14,22 +22,16 @@ then
   aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY ;
   aws configure set region us-east-1;
   aws configure set output json
-  Lambda_func_config='-frun dotnetcore1.0 -frole arn:aws:iam::275485857435:role/user-removal-service-lambda -fsub subnet-a88614e0,subnet-55db070f --function-security-groups sg-b9b543c8';
-  deploy_produnction $Lambda_func_config
+  Lambda_func_config='-frole arn:aws:iam::275485857435:role/user-removal-service-lambda -fsub subnet-a88614e0,subnet-55db070f --function-security-groups sg-b9b543c8';
+  if [$CIRCLE_BRANCH = "master"] then
+    deploy_produnction $Lambda_func_config
+  fi
+  if [$CIRCLE_BRANCH = "dev"] then
+    deploy_produnction $Lambda_func_config
+  fi
+  if [$CIRCLE_BRANCH = "staging"] then
+    deploy_produnction $Lambda_func_config
+  fi
 else
   dotnet build;
 fi
-
-
-
-deploy_development(){
-  dotnet lambda deploy-function LambdaFunCSharpDev --region us-east-1 -frole arn:aws:iam::275485857435:role/user-removal-service-lambda \
-  -fsub subnet-a88614e0,subnet-55db070f \
-  --function-security-groups sg-b9b543c8;
-}
-
-deploy_stagging(){
-  dotnet lambda deploy-function LambdaFunCSharpStag --region us-east-1 -frole arn:aws:iam::275485857435:role/user-removal-service-lambda \
-  -fsub subnet-a88614e0,subnet-55db070f \
-  --function-security-groups sg-b9b543c8;
-}
